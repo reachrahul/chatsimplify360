e03b3afc52cf747372a358f9e7da3f4971b5a83f
Rahul Saini <rahulsinghadia1412@gmail.com>
2023-11-22 18:58:11 +0530

    changed const

--- src/constants/list.js
+++ src/constants/list.js
@@ -1,4 +1,4 @@
-const  list = [{'title':'header 1'},{'title':'header 2'},{'title':'header 3'}]
+const  list = [{'title':'Header 1'},{'title':'header 2'},{'title':'header 3'}]
 
 const messages = [{'message':'message1','timestamp':1700629371},{'message':'message2','timestamp':1700629371},{'message':'message3','timestamp':1700629371},{'message':'message1','timestamp':1700629371}]
 
